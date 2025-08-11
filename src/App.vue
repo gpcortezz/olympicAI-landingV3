ea9d2a3991ff7df7611bf2076378bb8099eb56c2
gpcortezz <giovannipliego1006@gmail.com>
2025-08-11 21:16:28 +0000

    feat: remove docs

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,9 @@
 <script setup>
 import Landing from './components/Landing.vue';
-import GsapTest from './components/Gsap-test.vue';
 </script>
 
 <template>
   <Landing />
-  <!-- <GsapTest /> -->
 </template>
 
 <style scoped>
